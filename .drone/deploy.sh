219f5e0340e3f13d6bbafdca4c8bd4c893880f2a
Taylor Young <taylor@hyprtxt.com>
2015-11-11 12:50:03 -0700

    cleanup

--- .drone/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove the current site
-ssh root@45.55.237.204 'rm -Rf /var/www/afaa.hyprtxt.com/static_generated'
-# SCP the new build
-scp /var/cache/drone/src/bitbucket.org/sportsmedicine/afaa.hyprtxt.com/static_generated root@45.55.237.204:/var/www/afaa.hyprtxt.com/static_generated/
-echo 'Deploy Success!'
