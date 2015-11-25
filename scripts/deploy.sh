2216bc85755ebfe86cc562276ec6bc47c628233e
Taylor Young <taylor@hyprtxt.com>
2015-11-25 09:55:00 -0700

    deploy notification change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd ~/www/afaa.nasm.pw
+
 rsync -r --del ./static_generated nasm:/var/www/afaa.nasm.pw
 
 curl -X POST \
---data-urlencode 'payload={"text": "http://afaa.nasm.pw has been deployed.", "channel": "#code", "username": "deploy-bot", "icon_emoji": ":shipit:"}' \
+--data-urlencode 'payload={"text": "http://afaa.nasm.pw has been updated with the latest changes.", "channel": "#code", "username": "deploy-bot", "icon_emoji": ":shipit:"}' \
 https://hooks.slack.com/services/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
