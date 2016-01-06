888bc752a2cc3b60813065793186eb21a726ebee
Taylor Young <taylor@hyprtxt.com>
2016-01-06 15:16:20 -0700

    better

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rsync -r --del ./static_generated nasm:/var/www/afaa.nasm.pw
+# rsync -r --del ./static_generated nasm:/var/www/afaa.nasm.pw
+rsync -r ./static_generated nasm:/var/www/afaa.nasm.pw
 
 curl -X POST \
 --data-urlencode 'payload={"text": "http://afaa.nasm.pw has been updated with the latest changes.", "channel": "#code", "username": "deploy-bot", "icon_emoji": ":shipit:"}' \
