23a77a23c9c94661c5e2e15bd06d85deed23860c
Taylor Young <taylor@hyprtxt.com>
2016-02-26 14:08:36 -0700

    good

--- promo.js
+++ promo.js
@@ -1,19 +1,21 @@
-promo = $.ajax({
-  url: '//afaa-prd-mkt01/ajax/promo-bar',
-  method: 'GET'
-});
-promo.done(function(data) {
-  var $promoBar, $spacer;
-  $spacer = $('.mobile-nav-spacer');
-  $promoBar = $(data).find('#PublicWrapper .sfContentBlock');
-  $promoBar.insertAfter( $spacer );
-  setTimeout(function() {
-    $('.navbar-message').addClass('activated');
-    $('.navbar-message').on('click', function(e) {
-      $('#offer').modal();
-    });
-  }, 1000);
-});
-promo.error(function(err) {
-  return console.log(err);
+jQuery( function( $ ) {
+  promo = $.ajax({
+    url: '//afaa.nasm.pw/promo.html',
+    method: 'GET'
+  });
+  promo.done(function(data) {
+    var $promoBar, $spacer;
+    $spacer = $('.mobile-nav-spacer');
+    $promoBar = $( data )
+    $promoBar.insertAfter( $spacer );
+    setTimeout(function() {
+      $('.navbar-message').addClass('activated');
+      $('.navbar-message').on('click', function(e) {
+        $('#offer').modal();
+      });
+    }, 1000);
+  });
+  promo.error(function(err) {
+    return console.log(err);
+  });
 });
